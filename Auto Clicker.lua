891039eb85fb06312cbbb779d814fbbe40b396d4
aturner <69327501+BimbusCoder@users.noreply.github.com>
2021-09-05 23:28:15 -0500

    Update Auto Clicker.lua

--- Auto Clicker.lua
+++ Auto Clicker.lua
@@ -1,12 +1,3 @@
---[[
-getgenv().Settings = {
-    ["Auto Click Keybind"] = "V",
-    ["Lock Mouse Position Keybind"] = "B",
-    ["Right Click"] = false
-}
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BimbusCoder/Script/main/Auto%20Clicker"))()
-]]
-
 --Already Running--
 if getgenv()["Already Running"] then return else getgenv()["Already Running"] = "" end
 --Services--
@@ -26,7 +17,7 @@ local Mouse = setmetatable({}, {
 })
 --Get Keybind--
 local getKeycode = function(bind)
-    return pcall(function() return Enum.KeyCode[bind] end) and Enum.KeyCode[bind] or bind
+    return (pcall(function() return (Enum.KeyCode[bind]) end) and Enum.KeyCode[bind] or bind)
 end
 --Draw--
 local Draw = function(obj, props)
@@ -63,18 +54,25 @@ UIS.InputBegan:Connect(function(inputObj, GPE)
     end
 end)
 --Auto Click--
-RunService.RenderStepped:Connect(function()
-    Text.Position = Vector2.new(Camera.ViewportSize.X - 133, Camera.ViewportSize.Y - 48)
+coroutine.wrap(function()
+    while true do
+        Text.Visible = Settings.GUI
+        Text.Position = Vector2.new(Camera.ViewportSize.X - 133, Camera.ViewportSize.Y - 48)
 
-    if (flags.Auto_Clicking) then
-        if (flags.Mouse_Locked) then
+        if (flags.Auto_Clicking) then
             for i = 1, 2 do
-                VIM:SendMouseButtonEvent(flags.Mouse_Locked_Position.X, flags.Mouse_Locked_Position.Y, Settings["Right Click"] and 1 or 0, i == 1, nil, 0)
-            end
-        else
-            for i = 1, 2 do
-                VIM:SendMouseButtonEvent(Mouse.X, Mouse.Y, Settings["Right Click"] and 1 or 0, i == 1, nil, 0)
+                if (flags.Mouse_Locked) then
+                    VIM:SendMouseButtonEvent(flags.Mouse_Locked_Position.X, flags.Mouse_Locked_Position.Y, Settings["Right Click"] and 1 or 0, i == 1, nil, 0)
+                else
+                    VIM:SendMouseButtonEvent(Mouse.X, Mouse.Y, Settings["Right Click"] and 1 or 0, i == 1, nil, 0)
+                end
             end
         end
+
+        if (Settings.Delay <= 0) then
+            RunService.RenderStepped:Wait()
+        else
+            wait(Settings.Delay)
+        end
     end
-end)
+end)()
