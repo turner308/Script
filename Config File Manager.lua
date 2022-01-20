00309c7ade87d48462c71c1249b4eb4f358be294
aturner <69327501+BimbusCoder@users.noreply.github.com>
2022-01-19 18:52:26 -0600

    Update Config File Manager.lua

--- Config File Manager.lua
+++ Config File Manager.lua
@@ -5,16 +5,16 @@ local function create_folder(folder_name)
     if not isfolder(folder_name) then
         makefolder(folder_name)
     end
-    return listfiles(folder_name)
+    return folder_name
 end
 function library:config(args) --[folder,config_table,config_name]
     args.config_name = s_find(args.config_name, '.') == 1 and args.config_name .. '.txt' or args.config_name
     args.config_table = args.config_table or {}
-    local encoded_settings = http:JSONEncode(args.config_table)
     local folder = args.folder and create_folder(args.folder) or false
     local file_path = args.config_name
+    local encoded_settings = http:JSONEncode(args.config_table)
     if folder then
-        file_path = args.folder .. '\\' .. file_path
+        file_path = folder .. '\\' .. file_path
         if not isfile(file_path) then
             writefile(file_path, encoded_settings)
         end
